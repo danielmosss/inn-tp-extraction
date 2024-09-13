4c46cab7eb10ca63d7a0e148dd6e5398421cf507
Daniël <daniel.mosselman@outlook.com>
2024-09-13 16:16:22 +0200

    save

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import spacy
 from spacy import displacy
+from datetime import datetime
+
+text = "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a sentence.")
-displacy.serve(doc, style="dep")
+doc = nlp(text)
+html = displacy.render(doc, style="ent")
+
+# datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y-%H:%M:%S")
+print("date and time =", dt_string)
+
+with open("entities-{}.html".format(datetime), "w") as f:
+    f.write(html)
