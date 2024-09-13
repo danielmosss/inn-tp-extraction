b07cb3ea6ed16bba9a1eacb11270bd833c5b8649
Daniël <daniel.mosselman@outlook.com>
2024-09-13 16:27:35 +0200

    remove coments

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ import os
 
 models = ["en_core_web_sm", "nl_core_news_sm"]
 
-# Function to list and choose files from the input directory
 def get_input_file():
     input_folder = "input"
     files = os.listdir(input_folder)
-    
-    # Display files to the user
+
     print("Choose a file to process:")
     for i, file in enumerate(files):
         print(f"{i+1}. {file}")
-    
-    # Get the user's choice
+
     choice = int(input("Enter the file number: ")) - 1
     return os.path.join(input_folder, files[choice])
 
@@ -27,34 +24,33 @@ def choose_model():
     choice = int(input("Enter the model number: ")) - 1
     return models[choice]
 
-# Main processing function
 def process_file():
-    # Get the chosen input file
+    # Choose file
     input_file = get_input_file()
     input_filename = os.path.basename(input_file)
 
-    # Read the content from the file
+    # Read contents
     with open(input_file, 'r') as f:
         text = f.read()
     
-    # Load the spaCy model and process the text
+    # get model
     model = choose_model()
     nlp = spacy.load(model)
+
+    # Process the text
     doc = nlp(text)
-    
-    # Render the named entities as HTML
     html = displacy.render(doc, style="ent")
     
-    # Get the current date and time
+    # GEt time to put in file
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
     
-    # Output filename format with the original input filename and timestamp
+    # create filename
     output_folder = "output"
     output_filename = f"output-{input_filename}-{dt_string}.html"
     output_path = os.path.join(output_folder, output_filename)
     
-    # Save the output HTML file in the output directory
+    # write file to path
     with open(output_path, "w") as f:
         f.write(html)
     
